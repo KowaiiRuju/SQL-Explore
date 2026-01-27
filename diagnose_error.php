1a58dda242e50a26ae5eb5db58f153099873b045
KowaiiRuju <emman805@yahoo.com>
2026-01-27 09:41:24 +0800

    delted debug and updated readme

--- diagnose_error.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'PHPs/db.php';
-try {
-    get_pdo(true);
-    file_put_contents('error.txt', "Success: Connected.");
-} catch (Exception $e) {
-    file_put_contents('error.txt', "Error: " . $e->getMessage());
-    exit(1);
-}
